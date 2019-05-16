aa95e3406a378ef7648bdd6e3503e6bf36436bb3
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-05-16 08:50:00 +0200

    commit

--- ex2 - zest 4/Program.cs
+++ ex2 - zest 4/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ex2___zest_4
+namespace ConsoleApp7
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Human person = new Human();
+            person.Name = "John";
+            person.Surname = "Kowalski";
+            person.Age = 25;
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Surname);
+            Console.WriteLine(person.Age);
+
+            Console.ReadKey();
+        }
+        class Human
+        {
+            private string name, surname;
+            private int age;
+            public string Name { get => name; set => name = value; }
+            public string Surname { get => surname; set => surname = value; }
+            public int Age { get => age; set => age = value; }
         }
     }
 }
